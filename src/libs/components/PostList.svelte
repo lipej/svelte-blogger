44c10600e603a36d7e91458d5d8ca0b08f5bfa0e
Felipe José <lipejose@icloud.com>
2022-02-08 22:31:12 -0300

    feat: postlist component

--- src/libs/components/PostList.svelte
+++ src/libs/components/PostList.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
-	export let data: any[];
+	import type { Post } from 'src/graphql/generated/graphql';
+	import { dateFormatter } from 'src/utils';
+	export let data: Post[];
 </script>
 
-<div class="flex flex-col  pt-8">
+<div class="flex flex-col">
 	{#each data as post}
 		<div class="mx-4 mt-4">
 			<div>
-				<span class="font-bold text-2xl">{post.title}</span>
-				<br />
-				<span class="italic text-gray-400 text-sm"
-					>{new Date(post.createdAt).toLocaleString('pt-BR', {
-						year: 'numeric',
-						month: 'long',
-						day: 'numeric'
-					})}</span
-				>
+				<a href={'/posts/' + post.slug} class="font-bold text-2xl">{post.title}</a>
+				<p class="italic text-gray-400 text-sm">{dateFormatter(post.createdAt)}</p>
 			</div>
-			<p class="pt-2 text-justify text-base">{post.description}</p>
+			<p class="pt-2 text-justify text-base">{post.description ?? ''}</p>
 		</div>
 	{/each}
 </div>
