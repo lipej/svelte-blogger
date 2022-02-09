f0259a2637c2a220ea41464efffeaaaccfe8d98d
Felipe José <lipejose@icloud.com>
2022-02-08 22:28:30 -0300

    feat: about config

---
+++ src/config/about.ts
@@ -0,0 +1,15 @@
+export const about = {
+	name: 'Felipe José',
+	img: 'https://www.linkpicture.com/q/LPic620050670fb95945239599.jpg',
+	description: 'Desenvolvedor de Software',
+	skills: [
+		{ name: 'Typescript', percent: '90' },
+		{ name: 'Javascript', percent: '90' },
+		{ name: 'React', percent: '60' },
+		{ name: 'Svelte', percent: '40' },
+		{ name: 'Elixir', percent: '50' },
+		{ name: 'HTML', percent: '50' },
+		{ name: 'CSS', percent: '50' }
+	],
+	tags: ['Software', 'Developer', 'Tech']
+};
