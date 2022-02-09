4271361387e04ba8274a48dd51fdfec663601878
Felipe José <lipejose@icloud.com>
2022-02-08 22:31:57 -0300

    feat: layout

--- src/libs/components/Footer.svelte
+++ src/libs/components/Footer.svelte
@@ -2,7 +2,7 @@
 	export let copyrigth: string;
 </script>
 
-<footer class="p-4 footer bg-base-300 text-base-content footer-center block inset-x-0 bottom-0">
+<footer class="block inset-x-0 bottom-0 p-4 footer bg-base-300 text-base-content footer-center ">
 	<div>
 		<p>Copyright © 2021 - All right reserved by {copyrigth}</p>
 	</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,26 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	import { about } from 'src/config';
 	import '../app.css';
+	import Header from 'src/libs/components/Header.svelte';
+	import Footer from 'src/libs/components/Footer.svelte';
+	import AboutCard from 'src/libs/components/AboutCard.svelte';
+
+	onMount(() => {
+		themeChange(false);
+	});
 </script>
 
-<slot />
+<Header title="Lipe José | Dev" />
+
+<div class="grid grid-cols-3">
+	<div class="flex justify-center m-8">
+		<AboutCard {about} />
+	</div>
+	<div class="col-span-2 m-10">
+		<slot />
+	</div>
+</div>
+
+<Footer copyrigth="Lipe" />
