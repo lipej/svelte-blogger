f8b66ee140edb878f5f56f688dc090998a1b6d8e
lipe <80367187+lipejose@users.noreply.github.com>
2022-02-16 14:04:32 -0300

    fix: secure issue

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -3,8 +3,5 @@ import { SvelteApolloClient } from 'svelte-apollo-client';
 
 export const client = SvelteApolloClient({
 	uri: <string>import.meta.env.VITE_GRAPHCMS_HOST,
-	headers: {
-		Authorization: `Bearer ${<string>import.meta.env.VITE_GRAPHCMS_TOKEN}`
-	},
 	cache: new InMemoryCache()
 });
